e7bc304a1946b0ef12290d712e752c3de5770388
maureco-lab <mauriciomrm04@gmail.com>
2025-02-26 22:13:42 -0300

    Add files via upload

---
+++ Lista 1/Desafio.py
@@ -0,0 +1,8 @@
+numero = int(input("Dê um número:"))
+divisor = int(input("Dê um número divisor:"))
+
+resposta = numero//divisor
+resto = numero-(resposta*divisor)
+
+print("O resultado é:", resposta)
+print("O resto da divisão é:", resto)
